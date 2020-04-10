39b33a1a9a069088badf9b7966d162f531cb6309
Brian Reichholf <brian.reichholf@gmail.com>
2020-04-10 10:34:12 +0200

    Return to concat form

--- config/dynamic-routes.js
+++ config/dynamic-routes.js
@@ -2,11 +2,11 @@ const glob = require("glob")
 const path = require("path")
 
 export default function (urlFilepathTable) {
-  return Object.keys(urlFilepathTable)
-    .map((url) => {
+  return [].concat(
+    ...Object.keys(urlFilepathTable).map((url) => {
       return glob
         .sync(urlFilepathTable[url], { cwd: "content" })
         .map((filepath) => `${url}/${path.basename(filepath, ".md")}`)
     })
-    .flat()
+  )
 }
